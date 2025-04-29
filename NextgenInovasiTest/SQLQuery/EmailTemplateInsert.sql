44cbf6b8e3acf062d75818c888ce3cfb099569e1
syahrulMub <137327023+syahrulMub@users.noreply.github.com>
2025-04-30 00:31:22 +0700

    Update EmailTemplateInsert

--- NextgenInovasiTest/SQLQuery/EmailTemplateInsert.sql
+++ NextgenInovasiTest/SQLQuery/EmailTemplateInsert.sql
@@ -1,10 +1,7 @@
 INSERT INTO [dbo].[EmailTemplates]  (EmailCode, Subject, Body)
 VALUES
--- Daily Scheduler
 ('DAILY_REPORT', 'Daily Report Email', 'Hello {username}, This is your scheduled daily report. Please review the attached data.'),
 
--- Specific Date Scheduler
 ('ONETIME_REMINDER', 'Reminder Email for Event', 'Hello {username}, This is a one-time reminder for your upcoming scheduled event. {event}'),
 
--- Monthly Scheduler
 ('MONTHLY_SUMMARY', 'Monthly Summary Email', 'Hello {username}, Please send your report activity for this month');
